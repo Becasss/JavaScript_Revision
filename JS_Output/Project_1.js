55d4bf9686eb366c0d911a2e22b8f5073ef24c09
w3schools-test <raeebikash@gmail.com>
2024-07-13 10:23:18 +0545

    Day-2

---
+++ JS_Output/Project_1.js
@@ -0,0 +1,22 @@
+// Wriing into an HTML element, using innerHTML.
+
+document.getElementById("demo").innerHTML
+    = 5 + 9;
+
+
+// Writing into the HTML output using docuent.write():
+document.write(5 + 10);
+document.write("<h2>Demo</h2> <br> Hello Bikash Yamphu Rai");
+
+
+
+
+
+// Writing into the browser console, using console.log():
+
+console.log("Hello Bikash Yamphu");
+console.log(5 + 10);
+
+    // Writing into an alert box, using window.alert():
+    window.alert("Hello");
+window.alert(9 + 8);
