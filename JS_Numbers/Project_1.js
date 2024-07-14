f635a15ed869f2a9c8e8cc3ef42962f19f37114a
w3schools-test <raeebikash@gmail.com>
2024-07-14 08:03:42 +0545

    Update code

---
+++ JS_Numbers/Project_1.js
@@ -0,0 +1,24 @@
+let x = 34.289; // A number with decimals
+let y = 45; // A number without Decimals
+
+let sum = x + y;
+document.write("The total sums is " + sum);
+
+
+
+
+// Exponent Notation:
+let a = 123e5;
+let b = 123e-5;
+
+document.write(a + "<br>" + b);
+
+
+// Adding Nmbers and Strings:
+let c = "10";
+let d = "123";
+let e = c + d;
+
+document.write("The sum of c + d is " + e); // If you add two strings, the result will be concatenation.
+
+
