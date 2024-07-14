918e793f03ccffe050c25b0718f6f0efe5a54f82
w3schools-test <raeebikash@gmail.com>
2024-07-14 07:45:13 +0545

    Updating code

---
+++ JS_Variables/Project_1.js
@@ -0,0 +1,38 @@
+// Variables are containers for storing data:
+// In JS Variables can be declared in 4 ways: automatically, Using Var, Using let and Using const:
+
+// Let variables:
+/* Variables declared with let have Block Scope.
+ * They can't be accessed outside of the block where they are defined.
+ * They can't be redeclared or reassigned within the same block.
+ */
+
+let name = "Bikash";
+console.log(name); // Output: Bikash
+
+
+
+// Const variables:
+/** 
+ Variablesdefined with const cannot be Redeclared
+ variables defined with const cannot be reassigned
+ Variables defined with const have Block Scope.
+
+
+
+ // When to use JavaScript Const?
+ - Use consst when you declare:
+        - A new Array
+        - A new Object
+        - A new Function
+        - A new RegExp
+        
+ */
+
+const age = 25;
+console.log(age); // Output: 25
+
+
+
+
+// Var variables:
