8f8b2215bd12cc7f3e1f91532b41ae516d311553
w3schools-test <raeebikash@gmail.com>
2024-07-19 22:01:14 +0545

    Update project_1.js

---
+++ JS_Operators/Project_1.js
@@ -0,0 +1,42 @@
+// Addition Operators
+let a = 90;
+let x = (100 + 50) * a;
+document.write(x);
+
+
+
+// Subtraction Operators
+let b = 100;
+let y = b - 50;
+document.write("<br>" + y);
+
+// Multiply Operators
+let c = 10;
+let z = c * 5;
+document.write("<br>" + z);
+
+
+// Divide Operators
+let d = 100;
+let w = d / 5;
+document.write("<br>" + w);
+
+// Increment Operators
+let e = 10;
+e++;
+document.write("<br>" + e);
+
+let f = 10;
+++f;
+document.write("<br>" + f);
+
+
+
+// Decrement Operators
+let g = 10;
+g--;
+document.write("<br>" + g);
+
+let h = 10;
+--h;
+document.write("<br>" + h)
