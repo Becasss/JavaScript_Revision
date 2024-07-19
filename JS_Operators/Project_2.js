0b6c30eb491117232f037f74c359d44ba16cac0e
w3schools-test <raeebikash@gmail.com>
2024-07-19 22:09:50 +0545

    Update Project_2.js

--- JS_Operators/Project_2.js
+++ JS_Operators/Project_2.js
@@ -11,3 +11,24 @@ document.write(x);
 let y = 20;
 y -= 10; // y = y - 10
 document.write("<br>" + y);
+
+
+// Multiplication Assignment Operators
+
+let z = 5;
+z *= 10; // z = z * 10
+document.write("<br>" + z);
+
+//  Division Assignment Operators
+
+let w = 100;
+w /= 5; // w = w / 5
+document.write("<br>" + w);
+
+// Exponentiation Assignment Operators
+
+let a = 2;
+a **= 3; // a = a ** 3
+document.write("<br>" + a);
+
+// Remainder Assignment Operators
