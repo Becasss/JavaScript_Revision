7df400438dbdd2a771c5724994feecf8425eba45
w3schools-test <raeebikash@gmail.com>
2024-07-20 22:50:10 +0545

    Update Project_1.js

--- JS_DataTypes/Project_1.js
+++ JS_DataTypes/Project_1.js
@@ -2,3 +2,7 @@
 
 // String, Number, Bigint, Boolean, Undefined, Null, Symbol and Ojbet
 
+// Object data types can contain both built-in objects and user defined objects.
+// Objects, arrays, dates, maps, sets, intarrays, floatarrays, promises and more.
+
+
