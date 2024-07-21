3f031a6865f623f45bdab121028b94b7566b6866
w3schools-test <raeebikash@gmail.com>
2024-07-21 23:01:39 +0545

    Update Project_1.js

--- JS Functions/Project_1.js
+++ JS Functions/Project_1.js
@@ -3,3 +3,16 @@ function myFunctoin(p1, p2) {
 }
 let sum = myFunctoin(23, 34);
 document.write("<br>The sum of 23 and 34 is " + sum);
+
+
+
+// Funtion is called, the return value will end up in x
+let x = myfunction(5, 6);
+
+function myfunction(c, d) {
+    
+
+    return c * d;
+}
+
+document.write("<br>The prorduct of c and d is " + x);
