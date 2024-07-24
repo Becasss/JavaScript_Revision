6e5c9ca229c21fa8264ec038dd976f9f3d4134b4
w3schools-test <raeebikash@gmail.com>
2024-07-24 22:17:54 +0545

    Update Project_1.js

--- JS_Objects/Project_1.js
+++ JS_Objects/Project_1.js
@@ -10,3 +10,18 @@ let person = {
 
 // Accessing the properties of the object
 console.log('Hello, I am' + person.fastName + 'and I am ' + person.age + 'years old');
+
+
+
+
+// create an Object
+const person1 = {};
+
+// Add Properties
+person1.firstName = "Dhan Bahadur";
+person1.lastName = "Rai";
+person1.age = 60;
+person1.city = "Sankhuwasabha";
+
+document.write(person1.firstName + ' ' + person1.lastName + ' is ' + person1.age + " years old .");
+ 
