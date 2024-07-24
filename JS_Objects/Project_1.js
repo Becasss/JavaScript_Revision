f8df20a8fe41072f3802a6788fc6db84039a1867
w3schools-test <raeebikash@gmail.com>
2024-07-24 22:14:05 +0545

    Update Project_1.js

---
+++ JS_Objects/Project_1.js
@@ -0,0 +1,12 @@
+// Creating a JavaScript object using an Object literals
+
+// create an object
+let person = {
+    fastName: "Bikash",
+    lastName: "Yamphu",
+    age: 25,
+    city: "Kolkata"
+}
+
+// Accessing the properties of the object
+console.log('Hello, I am' + person.fastName + 'and I am ' + person.age + 'years old');
