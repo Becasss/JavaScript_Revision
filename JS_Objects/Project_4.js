f29ab6928e20f7ee21f2a527b68844078490512b
w3schools-test <raeebikash@gmail.com>
2024-07-27 22:08:08 +0545

    Update Project_4.js

--- JS_Objects/Project_4.js
+++ JS_Objects/Project_4.js
@@ -1,1 +1,16 @@
 // Javascript Object Constructor
+
+// Object Type Person
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;;
+
+}
+
+// create a Person Object
+const myFather = new Person("Bikash", "Yamphu Rai", 59, "blue");
+
+// Display age
+console.log("My father age is" + myFather.age + '.');
