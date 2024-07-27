27a5b852a1683369190688613a55e5c677b2c035
w3schools-test <raeebikash@gmail.com>
2024-07-27 22:35:23 +0545

    update project_4.js

--- JS_Objects/Project_4.js
+++ JS_Objects/Project_4.js
@@ -12,8 +12,7 @@ function Person(first, last, age, eye) {
 // create a Person Object
 const myFather = new Person("Bikash", "Yamphu Rai", 59, "blue");
 
-// Display age
-console.log("My father age is" + myFather.age + '.');
+
 
 
 // create two person objects
@@ -21,5 +20,7 @@ const myMother = new Person("Rania", "Yamphu Rai", 57, "brown");
 const myBrother = new Person("Rajesh", "Yamphu Rai", 55, "brown");
 
 // Display age
+// Display age
+console.log("My father age is" + myFather.age + '.');
 console.log("My mother age is" + myMother.age + '.');
 console.log("My brother age is" + myBrother.age + '.');
