dda0a06c8fb11f3034b3ee251e1c31eab2d6f8f3
w3schools-test <raeebikash@gmail.com>
2024-08-03 23:07:57 +0545

    Update Project_1.js

--- JS_Dates/Project_1.js
+++ JS_Dates/Project_1.js
@@ -15,4 +15,5 @@ new Date(year, month, day)
 new Date(year, month,day, hours)
 new Date(year, month, day, hours, minutes)
 new Date(year, month, day, hours, minutes, seconds)
+new Date(year, month, day, hours, minutes, seconds, milliseconds)
  */
