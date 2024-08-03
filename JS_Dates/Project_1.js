ac5e9c6d16266db29ff65f6c47fcd865bee19c87
w3schools-test <raeebikash@gmail.com>
2024-08-03 23:07:29 +0545

    U[date Project_1.js

--- JS_Dates/Project_1.js
+++ JS_Dates/Project_1.js
@@ -3,3 +3,16 @@ document.write(d);
 
 const f = new Date("2023-12-09");
 document.write("<br>" + f);
+
+
+// Creating Date Objects
+
+/** 
+ new Date()
+ new Date(date string)
+new Date(year, month)
+new Date(year, month, day)
+new Date(year, month,day, hours)
+new Date(year, month, day, hours, minutes)
+new Date(year, month, day, hours, minutes, seconds)
+ */
