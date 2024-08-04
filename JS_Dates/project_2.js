0560484a4dc6b0c0e60f9de56fcf7d1c6fe77c6f
w3schools-test <raeebikash@gmail.com>
2024-08-04 21:40:00 +0545

    Update Project_2.js

--- JS_Dates/project_2.js
+++ JS_Dates/project_2.js
@@ -1,2 +1,24 @@
 const d = new Date("2014-11-01T00:00:00");
 document.write(d.toString());
+
+
+
+/// JavaScript Date.parse()
+
+const msec = Date.parse("2014-11-01T00:00:00");
+document.write("<br>" + msec);
+
+
+
+// The new Date() Constructor
+// In javascript, date objects are created with new Date().
+
+// Get the current date
+const i = new Date();
+document.write("<br>" + i);
+
+
+// The getFullYear() Method:
+
+const a = new Date("2014-11-01T00:00:00");
+document.write("<br>" + a.getFullYear());
