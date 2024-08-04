b25354cf95a47327011016cb85b2c3f6f928b587
w3schools-test <raeebikash@gmail.com>
2024-08-04 21:46:08 +0545

    Update Project_2.js

--- JS_Dates/project_2.js
+++ JS_Dates/project_2.js
@@ -22,3 +22,24 @@ document.write("<br>" + i);
 
 const a = new Date("2014-11-01T00:00:00");
 document.write("<br>" + a.getFullYear());
+
+// The getMonth() Method:
+document.write("<br>" + a.getMonth());
+
+
+
+// The getMonth() Method:
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "March"];
+
+const w = new Date("2021-03-25");
+let month = months[w.getMonth()];
+document.write("<br>" + month);
+
+// The getDay() Method:
+document.write("<br>" + w.getDay());
+
+
+// The getHours() Method:
+document.write("<br>" + a.getHours());
+
+// The getMinutes() Method:
