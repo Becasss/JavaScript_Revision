6b586e53de95ad63f39f1e53ee5d881497a2983a
w3schools-test <raeebikash@gmail.com>
2024-08-06 22:05:23 +0545

    Update  Project_1.js

--- JS Maths/Project_1.js
+++ JS Maths/Project_1.js
@@ -4,8 +4,20 @@ document.write(Math.PI);
 
 // Maths Properties (Constants):
 
-document.write(Math.E);     // returns Euler's number
+document.write("<br>" + Math.E);     // returns Euler's number
 
-document.write(Math.PI);    // returns PI
+document.write("<br>" + Math.PI);    // returns PI
 
-document.write(Math.SQRT2);  // returns the square root of 2
+document.write("<br>" + Math.SQRT2);  // returns the square root of 2
+
+
+document.write("<br>" + Math.SQRT1_2); // returns the square root of 1/2
+
+
+document.write("<br>" + Math.LN2);      // returns the natural Logarithm of 2
+
+document.write("<br>" + Math.LN10);     // returns the natural Logarithm of 10 
+
+document.write("<br>" + Math.LOG2E); // returns the base 2 logarithm of E
+
+document.write("<br>" + Math.LOG10E); // returns the base 10 logarithm of E
