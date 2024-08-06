024e6e7cbd1ec17592a2ae2b5bdd73b5b2985bda
w3schools-test <raeebikash@gmail.com>
2024-08-06 22:00:14 +0545

    Update Project

--- JS Maths/Project_1.js
+++ JS Maths/Project_1.js
@@ -1,1 +1,11 @@
 document.write(Math.PI);
+
+
+
+// Maths Properties (Constants):
+
+document.write(Math.E);     // returns Euler's number
+
+document.write(Math.PI);    // returns PI
+
+document.write(Math.SQRT2);  // returns the square root of 2
