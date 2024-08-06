356c3fab5b0e49cc9520cd7daa86d2674d8c22d3
w3schools-test <raeebikash@gmail.com>
2024-08-06 22:21:39 +0545

    Update Project_1.js

--- JS Maths/Project_1.js
+++ JS Maths/Project_1.js
@@ -21,3 +21,23 @@ document.write("<br>" + Math.LN10);     // returns the natural Logarithm of 10
 document.write("<br>" + Math.LOG2E); // returns the base 2 logarithm of E
 
 document.write("<br>" + Math.LOG10E); // returns the base 10 logarithm of E
+
+
+
+
+// Math.methods(number):
+
+document.write("<br>" + Math.round(4.6));    // returns its nearest integer
+
+document.write("<br>" + Math.ceil(4.5));      // rounded up to its nearest integer
+
+document.write("<br>" + Math.floor(4.9));      // rounded down to its nearest integer
+
+
+document.write("<br>" + Math.trunc(4.9));       // returns the integer part of x
+    
+document.write("<br>" + Math.sign(-6));         // returns ths sign of  x is negative, null or positive
+
+
+document.write("<br>" + Math.pow(5, 3));      // returns the value of x to the power of y
+
