631053f75ae0bb08346257add6bded7bcf7b3a84
w3schools-test <raeebikash@gmail.com>
2024-08-07 21:24:01 +0545

    Update JS_Random.js

--- JS Maths/JS_Random.js
+++ JS Maths/JS_Random.js
@@ -18,3 +18,8 @@ document.getElementById("momo").innerHTML =
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
