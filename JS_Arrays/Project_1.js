e52a107dcc85ed7565f16d54b9665f6f4855f926
w3schools-test <raeebikash@gmail.com>
2024-08-20 22:38:47 +0545

    Update project_1.js

--- JS_Arrays/Project_1.js
+++ JS_Arrays/Project_1.js
@@ -20,3 +20,8 @@ const car = new Array('Mercedes', 'tractor', 'Jeeper',);
 let carr = car[0];
 
 console.log(carr); // Output:Mercedes
+
+
+// Changing array elements
+cars[0] = "Toyota";
+console.log(cars); // Output: Toyota, Volvo, BWM
