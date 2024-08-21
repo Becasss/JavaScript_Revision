15c7f480c5a4208b2ec84f508b0ddcc0c9819976
w3schools-test <raeebikash@gmail.com>
2024-08-21 23:10:16 +0545

    Update Project_2.js

--- JS_Arrays/Project_2.js
+++ JS_Arrays/Project_2.js
@@ -27,4 +27,24 @@ console.log(`second animal: ${secondAnimal}`);
 
 // JS Array join()
 let joinedAnimal = animals.join("*");
-console.log(`joined animals: ${joinedAnimal}`); // Joining all elements of animals with *
+console.log(`joined animals: ${joinedAnimal}`); // Joining all elements of animals with 
+
+
+// JS Array Pop()
+
+let lastAnimal = animals.pop();
+console.log(`last animal: ${lastAnimal}`); // Removing last element from animals
+console.log(`animals after pop: ${animals}`);
+
+
+/// JS Array Push()
+
+animals.push("Tiger");
+console.log(`animals after push: ${animals}`);
+
+
+// JS Array Shift().
+
+let firstAnimal = animals.shift();
+console.log(`first animal: ${firstAnimal}`); // Removing first element from animals
+console.log(`animals after shift: ${animals}`);
