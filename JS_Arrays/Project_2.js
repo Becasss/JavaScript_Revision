85d6305a015ff6e7c2e018b6aaddb5d35c6df097
w3schools-test <raeebikash@gmail.com>
2024-08-21 21:32:13 +0545

    Update Project_2.js

--- JS_Arrays/Project_2.js
+++ JS_Arrays/Project_2.js
@@ -3,3 +3,11 @@
 const fruits = ["Apple", "Banana", "Orange", "Mango"];
 let length = fruits.length;
 console.log(`length: ${length}`);
+
+
+
+// JS Array toString()
+
+const animals = ["Lion", "Leopard", "Dog", "Cat"];
+let animalsString = animals.toString();
+console.log(`animals: ${animalsString}`);
