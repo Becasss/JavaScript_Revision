a25858dc7ef2552f9fc6133716fae154a6fa2b80
w3schools-test <raeebikash@gmail.com>
2024-08-22 22:43:09 +0545

    Update project_3.js

--- JS_Arrays/Project_3.js
+++ JS_Arrays/Project_3.js
@@ -147,3 +147,28 @@ function displayCars(){
 
 
 // Comparing string properties is a little more complex.
+const carss = [
+    {type:"Volvo", year: 2016, color: "black"},
+    {type:"Saab", year: 2001, color: "white"},
+    {type:"BMW", year: 2010, color: "red"}
+];
+
+displayCarss();
+
+function myyFunction(){
+    carss.sort(function(a,b){
+        let x = a.type.toLowerCase();
+        let y = b.type.toLowerCase();
+        if(x<y) {return -1;}
+        if(x > y) {return 1;}
+        return 0;
+    });
+    displayCarss();
+}
+
+function displayCarss(){
+    document.getElementById('dodo').innerHTML = 
+    carss[0].type + " " + carss[0].year + " " + carss[0].color + "<br>" +
+    carss[1].type + " " + carss[1].year + " " + carss[1].color + "<br>" +
+    carss[2].type + " " + carss[2].year + " " + carss[2].color;
+}
