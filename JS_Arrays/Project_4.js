8ff526d30e028c20830d1e3fe50af9a74c7e3dd7
w3schools-test <raeebikash@gmail.com>
2024-08-22 23:21:46 +0545

    Update Project_4.js

--- JS_Arrays/Project_4.js
+++ JS_Arrays/Project_4.js
@@ -49,3 +49,32 @@ console.log(`the sum is:${sum}`);
 function functions(total, value, index, array){
     return total + value;
 }
+
+// JavaScript Array reduceRight();
+
+let sumRight = numbers.reduceRight(fncts);
+console.log(`the sum right is:${sumRight}`);
+
+function fncts(total, value, index, array){
+    return total + value;
+}
+
+
+// JavaScript Array every();
+
+let allEven = numbers.every(isEven);
+console.log(`Is even is:${allEven}`);
+
+
+function isEven(value, index, array){
+    return value % 2 == 0;
+}
+
+// Javascript Array some();
+
+let someOver5 = numbers.some(Over);
+console.log(`Is over 5 is:${someOver5}`);
+
+function Over(value, index, array){
+    return value > 5;
+}
