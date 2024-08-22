3d91966cb46570e3239b07adff3692e2e028b6ef
w3schools-test <raeebikash@gmail.com>
2024-08-22 14:26:27 +0545

    Update Project_3.js

--- JS_Arrays/Project_3.js
+++ JS_Arrays/Project_3.js
@@ -3,3 +3,10 @@
 const fruits  = ["Banana", "Coffee", "Olives", "Mango", "Jerry"];
 let sorted = fruits.sort();
 console.log(`sorted fruits: ${sorted}`);
+
+
+// Reversing an array
+
+let reversed = fruits.reverse();
+console.log(`reversed fruits: ${reversed}`);
+
