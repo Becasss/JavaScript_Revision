80f108477ccbc67da9ad1ee41ce80a6b20c88e76
w3schools-test <raeebikash@gmail.com>
2024-08-23 14:17:04 +0545

    Update Project_1.js

---
+++ JS Comparisons/Project_1.js
@@ -0,0 +1,15 @@
+// The && Operator (Logical AND):
+
+let a = 6;
+let b = 3;
+
+console.log(a < 10 && b >  1);
+console.log(a < 10 && b < 1);
+
+
+// The || Operator (Logical OR):
+
+console.log(a == 5 || b == 5);
+console.log(a == 6 || b == 0);
+console.log(a == 0|| b == 3);
+console.log(a == 6 || b == 3); 
