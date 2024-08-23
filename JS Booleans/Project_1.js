d60c97ec36a3a1f21719e8f1e9e59d5a70bff295
w3schools-test <raeebikash@gmail.com>
2024-08-23 12:59:12 +0545

    Update Project_1.js

--- JS Booleans/Project_1.js
+++ JS Booleans/Project_1.js
@@ -1,3 +1,12 @@
 const express = Boolean(10>9);
 
 console.log(express);
+
+
+
+// Everything with a value is True
+
+console.log(Boolean(100));
+console.log(Boolean(3.4));
+console.log(`Any (not empty) string is ${Boolean("any")}`);
+console.log(`any expression is (except zero) ${Boolean(1+3+3.14)}`);
