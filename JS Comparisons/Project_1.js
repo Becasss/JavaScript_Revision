70a6842eb348080bfbc29b664d4c1eff0c8b8285
w3schools-test <raeebikash@gmail.com>
2024-08-23 14:21:23 +0545

    Update Project_1.js

--- JS Comparisons/Project_1.js
+++ JS Comparisons/Project_1.js
@@ -13,3 +13,13 @@ console.log(a == 5 || b == 5);
 console.log(a == 6 || b == 0);
 console.log(a == 0|| b == 3);
 console.log(a == 6 || b == 3); 
+
+
+// Condition Ternary Operator
+
+// syntax : Variabelname = (condition) ? value1:value2
+
+let z = (a == b) ? "a and b are equal" : "a and b are not equal";
+console.log(z);
+
+
