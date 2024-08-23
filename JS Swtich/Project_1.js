f74e9a0669cd23f6e6361ec8f087d46bf4fedd45
w3schools-test <raeebikash@gmail.com>
2024-08-23 23:20:04 +0545

    Update Project_1.js

--- JS Swtich/Project_1.js
+++ JS Swtich/Project_1.js
@@ -40,3 +40,20 @@ switch(new Date().getDay()) {
 }
 
 console.log(text);
+
+//Strict Comparison
+
+let a = "0";
+
+switch (x) {
+    case 0:
+        text = "Off";
+        break;
+    case 1:
+        text = "On";
+        break;
+    default:
+        text = "No value found!";
+}
+
+console.log(text);
