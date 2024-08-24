613cfb88ecb39a993d350a626143efeccb89d74b
w3schools-test <raeebikash@gmail.com>
2024-08-24 12:06:16 +0545

    Update whileLoop.js

--- JS loops/whileLoop.js
+++ JS loops/whileLoop.js
@@ -5,3 +5,31 @@ while (i < 10){
     i ++;
 }
 console.log(text);
+
+
+
+
+// do While Loop:
+
+let txt = '';
+let a = 0;
+do{
+    txt += `\n The number is : ${a}`;
+    a++;
+}
+while (a < 10);
+console.log(txt);
+
+
+
+// Comparing FOR and while Loop:
+
+const cars = ["A", "B", "C", "D", "E", "F"];
+
+let b = 0;
+let trt = "";
+for (;cars[i];){
+    trt += cars[b] + "\n";
+    b++;
+}
+console.log(trt);
