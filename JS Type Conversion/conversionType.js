6c6d89dc4e34687c1382c0e37d843d34b3857f56
w3schools-test <raeebikash@gmail.com>
2024-08-24 20:19:19 +0545

    Update ConversionType

--- JS Type Conversion/conversionType.js
+++ JS Type Conversion/conversionType.js
@@ -10,3 +10,19 @@ Number("   ")+ "<br>" + Number("") + "<br>" +
 Number("99 33") + "<br>" + 
 Number("Johb") + "<br>" +
 Number("10") + "<br>" ;
+
+
+// The Uniary + operator
+
+let y = "5";
+let x =  + y;
+console.log(typeof y);
+console.log(typeof x);
+
+
+// Converting Numbers to Strings
+
+ let a = 123;
+ console.log(String(a));
+ console.log(String(123));
+console.log(100 + 24);
