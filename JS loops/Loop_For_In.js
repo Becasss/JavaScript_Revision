3115d2853c03e5d45f4d46848858753a1ab8958f
w3schools-test <raeebikash@gmail.com>
2024-08-24 11:39:18 +0545

    Update loopForIn.js

--- JS loops/Loop_For_In.js
+++ JS loops/Loop_For_In.js
@@ -7,7 +7,7 @@
  }
  */
 
-
+// For In Over Object:
  const person = {fname: 'John',
     lname: 'Doe',
     age: 30
@@ -15,6 +15,17 @@
 
  let text = "";
  for(let x in person) {
-    text += person[x].name;
+    text += person[x] + "\n";
  }
  console.log(text);
+
+
+ // For In Over Array:
+
+ const numbers = ["45", "4 ", "67", " 20", "100"];
+
+ let txt = "";
+ for(let x in numbers) {
+   txt += numbers[x] + "\n";
+ }
+ console.log(txt);
