8d4a6a84c014c87910c05de1e3624d0fb0243ee5
w3schools-test <raeebikash@gmail.com>
2024-08-24 20:14:51 +0545

    Update conversionType

---
+++ JS Type Conversion/conversionType.js
@@ -0,0 +1,12 @@
+// Converting strings to Numbers
+ 
+
+// Number() Method:
+
+document.getElementById("demo").innerHTML = 
+Number("3.14") + "<br>" + 
+Number(Math.PI) + "<br>" +
+Number("   ")+ "<br>" + Number("") + "<br>" + 
+Number("99 33") + "<br>" + 
+Number("Johb") + "<br>" +
+Number("10") + "<br>" ;
