137cb0817664241279acfc341dacbb84569e0fc8
w3schools-test <raeebikash@gmail.com>
2024-08-24 18:27:59 +0545

    Update Proejct1.js

--- JS Maps/Project_1.js
+++ JS Maps/Project_1.js
@@ -10,3 +10,22 @@ const fruits = new Map([['apples', 500],
 // Display the Map
 let numb = fruits.get("apples");
 console.log(numb); // Output: 500
+
+
+
+// The Set() Method - to creae a Set:
+
+const Products = new Map();
+
+// Set Map Values
+Products.set("apples",500);
+Products.set("Oranges",300);
+Products.set("Bananas",200);
+
+let nmb = Products.get("Bananas");
+console.log(nmb); // Output: 200
+
+
+// The get() Method - gets  the value of a key in a Map
+
+console.log(Products.get("Oranges")); // <Output:300></Output:300>
