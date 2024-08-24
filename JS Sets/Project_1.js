0782d878bb7d8dd94aefc58f22710c5db161bd63
w3schools-test <raeebikash@gmail.com>
2024-08-24 14:59:47 +0545

    Update Project_1.js

--- JS Sets/Project_1.js
+++ JS Sets/Project_1.js
@@ -5,3 +5,21 @@ const letters = new Set([1, 2, 3, 4, 5]);
 // Display set.size
 
 console.log(`The set has ${letters.size} keys.`);
+
+
+// Create a set and add values:
+
+const numbers = new Set();
+
+/// Add values to the set
+numbers.add("1");
+numbers.add(2);
+numbers.add(3);
+numbers.add(4);
+numbers.add(5);
+
+// Display the set
+
+
+console.log(numbers);
+console.log(numbers.size);
