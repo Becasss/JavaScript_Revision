022332b31ca0889be9158bbc236383887be77b5a
w3schools-test <raeebikash@gmail.com>
2024-08-24 19:02:40 +0545

    Update mapMethod

--- JS Maps/mapMethods.js
+++ JS Maps/mapMethods.js
@@ -5,6 +5,47 @@ const fruits = new Map([
     ["Bananas", 300],
     ["Cherry", 200]
 ]);
+fruits.set('apples',600);
 
-let numb = fruits.get("apples");
+let numb = fruits.get("apples"); // Map.get Method
 console.log(numb);
+
+
+// The Map.set() Method:
+const fruit = new Map();
+
+// set Map values
+fruit.set("Jery", 500);
+fruit.set("Kiwi", 350);
+fruit.set("Mango", 450);
+
+// Map.delete() Method:
+fruit.delete("Kiwi");
+
+let nmb = fruit.get("Jery");
+console.log(nmb);
+
+
+
+// Map.size() Method:
+console.log(fruit.size);
+console.log(fruits.size);
+
+// Map.clear() Method:
+fruits.clear();
+console.log(fruits.size);
+
+
+// Map.forEach() Method:
+const cities = new Map([
+    ["kathmandu", 500],
+    ["Tokyo", 600],
+    ['Bombay', 700]
+]);
+
+let txt = "";
+cities.forEach(function(value,key){
+    txt += key + ' = ' + value + "<br>";
+})
+document.getElementById("demo").innerHTML
+= txt;
