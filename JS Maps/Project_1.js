51191289f65d68206b955f4d4e99289ea483c462
w3schools-test <raeebikash@gmail.com>
2024-08-24 18:22:56 +0545

    Updating the Project-1

---
+++ JS Maps/Project_1.js
@@ -0,0 +1,12 @@
+// The new Map() Method  - to creae a Map:
+
+// Create a Map
+
+const fruits = new Map([['apples', 500],
+    ["Oranges", 300],
+    ["Bananas", 200]
+]);
+
+// Display the Map
+let numb = fruits.get("apples");
+console.log(numb); // Output: 500
