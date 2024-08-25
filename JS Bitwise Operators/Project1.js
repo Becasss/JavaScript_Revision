b86a3b2378da1c2a91c295ddc84c71f6678e5bfe
w3schools-test <raeebikash@gmail.com>
2024-08-25 13:55:19 +0545

    Converting Decimal to string

--- JS Bitwise Operators/Project1.js
+++ JS Bitwise Operators/Project1.js
@@ -24,3 +24,12 @@ console.log(5 << 2);
 // Javascript (Sign Preserving ) Bitwise Right Shift(>>)
 console.log(5 >> 1);
 console.log(5 >> 2);
+
+
+
+// Converting Decimal to Binary
+
+document.getElementById("emo").innerHTML = dec2bin(-5);
+function dec2bin(dec2bin) {
+    return(dec >> 0).toString(2);
+}
