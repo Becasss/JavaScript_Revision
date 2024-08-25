5715532d9f8a07de8a8a314afb5ed2de2fe84992
w3schools-test <raeebikash@gmail.com>
2024-08-25 12:13:40 +0545

    Update Project_1.js

--- Object Destructing/Project_1.js
+++ Object Destructing/Project_1.js
@@ -11,3 +11,20 @@ let {firstName, lastName} = person;
 // Display the primitive Values
 console.log(firstName);
 console.log(lastName);
+
+
+// Object Default Values:
+
+const people = {
+    fname : " Bikash",
+    lname: "Yamphu Rai",
+    age : 27
+}
+
+// Destructuring
+let {fname,lname, country = "US"} = people;
+
+// Display the primitive values
+console.log(fname);
+console.log(lname);
+console.log(country);
