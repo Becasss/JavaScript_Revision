375c247d2f5e1c18211fe37dacbfd57577e9e4e1
w3schools-test <raeebikash@gmail.com>
2024-08-25 15:48:55 +0545

    Update Prjeoct_1.js

--- JS Errors/Project_1.js
+++ JS Errors/Project_1.js
@@ -22,6 +22,30 @@ function myFunction(){
     }
     catch(err){
         message.innerHTML= "Input error: " + err;
-      
+ 
+    }
+}
+
+
+// The Finally Statement
+
+function myfunction(){
+    const mess = document.getElementById('p02');
+    mess.innerHTML= "";
+
+    let a = document.getElementById("momo").value;
+    try{
+        if(a.trim() == "") throw "empty";
+        if(isNaN(a)) throw "not a nubmer";
+        a = Number(a);
+        if(a < 5) throw "too low";
+        if (a > 10) throw "too high";
+
+    }
+    catch(err){
+        mess.innerHTML= "Input error: " + err;
+    }
+    finally{
+    document.getElementById('momo').value = "";
     }
 }
