c4fbf036268b2cf6bc3c884e0cebbef8160085bb
w3schools-test <raeebikash@gmail.com>
2024-08-26 10:32:42 +0545

    Adding arrow function

--- JS Function Arrow/Project_1.js
+++ JS Function Arrow/Project_1.js
@@ -11,3 +11,19 @@ hello = function(){
     return "Hello, world!";
 }
 console.log(hello());
+
+
+// With Arrow Function
+let hlo = "";
+
+hlo = () =>  {
+    return "Hello, world!";
+}
+console.log(hlo());
+
+
+
+// With Arrow Function
+let add = "";
+add = () => "Hello, world!";
+console.log(add());
