45832ad08f04a7cff908cb6d9300d42f53b90e2d
w3schools-test <raeebikash@gmail.com>
2024-08-26 11:10:53 +0545

    creating JS arrow function with this keyword

--- JS Function Arrow/Project_1.js
+++ JS Function Arrow/Project_1.js
@@ -59,3 +59,18 @@ window.addEventListener("load", maya);
 // A button object calls the functions:
 
 document.getElementById('btn').addEventListener("click",maya);
+
+
+
+
+// Javascript "this" with arrow functions
+let maya2 = "";
+maya2 = () => {
+    document.getElementById("demo2").innerHTML += maya2;
+}
+
+// The window object calls the function:
+window.addEventListener("load",maya2);
+
+// A button object calls the functions:
+document.getElementById('btn2').addEventListener("click",maya2);
