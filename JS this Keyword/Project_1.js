6cbdb7735fc894d1a11592b875402224d04160ea
w3schools-test <raeebikash@gmail.com>
2024-08-26 10:12:56 +0545

    Update Project_1.html

--- JS this Keyword/Project_1.js
+++ JS this Keyword/Project_1.js
@@ -22,3 +22,37 @@ let y = this;
 console.log(y); // undefined
 
 
+// this in a function (Default)
+function myFunction(){
+    return this;
+}
+console.log(myFunction());
+
+
+// this in a function (strict mode)
+
+
+"use strict";
+function myfunction(){
+    return this;
+}
+
+console.log(myfunction());
+
+
+/// this in Event Handler
+
+
+// Explicit Function Binding
+
+const person1 = {
+    fullName:function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+const person2 = {
+    firstName: "John",
+    lastName: "Doe"
+}
+let z = person.fullName.call(person2);
+console.log(z);
