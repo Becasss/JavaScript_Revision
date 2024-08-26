5e98e031df168e3368ed0411bc01fa76a0879cba
w3schools-test <raeebikash@gmail.com>
2024-08-26 11:50:44 +0545

    Update Project_1.js

--- JS Classes/Project_1.js
+++ JS Classes/Project_1.js
@@ -24,3 +24,20 @@ const myCar2 = new car("Audi", 2019);
 
 console.log(myCar1);
 console.log(myCar2);
+
+
+
+// Javascript Class Methods
+
+class Car {
+    constructor(name,year){
+        this.name = name;
+        this.year = year;
+    }
+age(){
+    const date = new Date();
+    return date.getFullYear() - this.year;
+}}
+
+const myCar = new Car("Ford", 2014);
+console.log(`The age of car is :${myCar.age()} years old.`);
