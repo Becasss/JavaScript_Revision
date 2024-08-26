779e69f451623813e306bd8414fe0cb1d1f9190f
w3schools-test <raeebikash@gmail.com>
2024-08-26 11:29:28 +0545

    Updating High Order Functions and callbacks

--- JS Function Arrow/ArrowFunctions.js
+++ JS Function Arrow/ArrowFunctions.js
@@ -30,3 +30,9 @@ console.log(greetPerson("Bikash"));
 
 const makePerson = (firstName, lastName) => ({first: firstName, last: lastName});;
 console.log(makePerson("Bikash", "Yamphu Rai"));
+
+
+// High Order Function and Callbacks
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled);
