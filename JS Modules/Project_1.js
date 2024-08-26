68b7219539bec6d98cb720a6db92cec01bdb7e0e
w3schools-test <raeebikash@gmail.com>
2024-08-26 12:43:30 +0545

    Update Projec_1.js

--- JS Modules/Project_1.js
+++ JS Modules/Project_1.js
@@ -1,4 +1,8 @@
-import { age, name } from "./person.js";
-
+import { age, Age, name, Name } from "./person.js";
 let text = `My name is ${name} and my age is ${age} years old.`;
 console.log(text);
+
+
+let text2 = `My name is ${Name} and my age is ${Age} years old.`;
+console.log(text2);
+
