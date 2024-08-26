2fe0f4f0ddf356ba071a8cb149a1e7f987f28558
w3schools-test <raeebikash@gmail.com>
2024-08-26 10:26:44 +0545

    Update Project_1.js

--- JS Function Arrow/Project_1.js
+++ JS Function Arrow/Project_1.js
@@ -1,3 +1,13 @@
 let myFunction = (a,b) => a * b;
 console.log(myFunction(4,6));
 
+
+
+
+// Before Arrow
+
+let hello = "";
+hello = function(){
+    return "Hello, world!";
+}
+console.log(hello());
