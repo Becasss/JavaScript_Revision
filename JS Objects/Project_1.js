4256bc2b54eb843cfb4f693da78073ea4408e839
w3schools-test <raeebikash@gmail.com>
2024-08-27 17:00:44 +0545

    Update Project_1.js

---
+++ JS Objects/Project_1.js
@@ -0,0 +1,11 @@
+// Javascript Object Literal
+
+const person = {};
+
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 30;
+person.eyeColor = "blue";
+
+// Display Data form Object
+console.log(`My name is ${person.firstName} ${person.lastName} ${person.age}.`)
