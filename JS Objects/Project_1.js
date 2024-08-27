6a8ec09606219a4d300349077c278106a7f65e71
w3schools-test <raeebikash@gmail.com>
2024-08-27 17:02:44 +0545

    Adding new Object() method

--- JS Objects/Project_1.js
+++ JS Objects/Project_1.js
@@ -8,4 +8,18 @@ person.age = 30;
 person.eyeColor = "blue";
 
 // Display Data form Object
-console.log(`My name is ${person.firstName} ${person.lastName} ${person.age}.`)
+console.log(`My name is ${person.firstName} ${person.lastName}.`);
+
+
+// Javascript new Object() Method:
+
+const person1 = new Object();
+
+// Add Properties
+person1.firstName = "Jane";
+person1.lastName = "Doe";
+person1.age = 25;
+person1.eyeColor = "brown";
+
+// Display Data from Object.
+console.log(`My name is ${person1.firstName} ${person1.lastName}.`);
