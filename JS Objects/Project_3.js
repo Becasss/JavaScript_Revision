9b0d25216a8894ed9f4afb896ff42e753d64a275
w3schools-test <raeebikash@gmail.com>
2024-08-28 13:15:49 +0545

    Updating Project3.js

--- JS Objects/Project_3.js
+++ JS Objects/Project_3.js
@@ -28,3 +28,37 @@ const person1 = {
 person.lang = "en";
 
 console.log(person.language);
+
+
+
+// Object.defineProperty()
+// Define an Object
+const obj = { couner: 0 };
+
+// Define setters and getters
+Object.defineProperty(obj, "reset", {
+    get: function () { this.counter = 0; }
+});
+
+Object.defineProperty(obj, "increment", {
+    get: function () { this.counter++; }
+});
+Object.defineProperty(obj, "decrement", {
+    get: function () { this.counter--; }
+});
+Object.defineProperty(obj, "add", {
+    set: function (value) { this.counter += value; }
+});
+Object.defineProperty(obj, "substract", {
+    set: function (value) { this.counter -= value; }
+});
+
+
+// play with counter
+obj.reset;
+obj.add = 5;
+obj.substract = 1;
+obj.increment;
+obj.decrement;
+
+console.log(obj.counter); // Output: 4
