d38921246fc5501a2efe44021ef65d5606bdd777
w3schools-test <raeebikash@gmail.com>
2024-08-29 13:24:35 +0545

    Update Asynchronous.js

--- JS Async/JS_Asynchronous.js
+++ JS Async/JS_Asynchronous.js
@@ -5,3 +5,10 @@ setTimeout(myFunction, 3000);
 function myFunction() {
     console.log("I love You!");
 }
+
+
+setTimeout(function () { myfunction("I lvove your!"); }, 3000);
+
+function myfunction(value) {
+    console.log(value);
+}
