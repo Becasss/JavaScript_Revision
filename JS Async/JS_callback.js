6b7ad9e8366c84009062acbf0f4b745a29e22578
w3schools-test <raeebikash@gmail.com>
2024-08-29 11:13:47 +0545

    Update Sequence control

--- JS Async/JS_callback.js
+++ JS Async/JS_callback.js
@@ -14,3 +14,19 @@ function mysecond() {
 
 mysecond();
 myfirst();
+
+
+
+// Sequence Control
+
+function myDisplay(some) {
+    console.log(some);
+}
+
+function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+
+    return sum;
+}
+let result = myCalculator(10, 10);
+myDisplay(result);
