7100aacf8f0d70369d59794a06e6e71bf55ec902
w3schools-test <raeebikash@gmail.com>
2024-08-29 14:38:55 +0545

    Update Promises.js

--- JS Async/JS_Promises.js
+++ JS Async/JS_Promises.js
@@ -1,13 +1,16 @@
 /* Promise Syntax
 let myPromise = new Promise(function(myResolve,myReject){
-// "Producing code" (May take some time
+// "Producing code" (May take some time)
 
 myResolve(); // when successful
 myReject();  // when error
 });
+
 // "Consuming Code"  (Must wait for a fulfilled promise)
+
 myPromise.then(
 function(value){//code if successful},
 function(error){// code if some error}
 );
+
 */ 
