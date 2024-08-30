8d90e9b587dd4b5c6d1022fb2712e6a6ef07555f
w3schools-test <raeebikash@gmail.com>
2024-08-30 17:25:36 +0545

    Update domAnimations

--- JS HTML DOM/domCss.js
+++ JS HTML DOM/domCss.js
@@ -6,3 +6,9 @@ document.getElementById('p1').style.color
 document.getElementById('p2').style.color = 'blue';
 document.getElementById('p2').style.fontFamily = "Arial";
 document.getElementById('p2').style.fontSize = "20px";
+
+
+
+
+// Using Events
+
