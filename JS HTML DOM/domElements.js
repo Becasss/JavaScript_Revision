e63e78fee1b615181d1a39a91c471d31f0691b8a
w3schools-test <raeebikash@gmail.com>
2024-08-30 14:43:58 +0545

    Update domElements

--- JS HTML DOM/domElements.js
+++ JS HTML DOM/domElements.js
@@ -19,3 +19,13 @@ const y = x.getElementsByTagName("p");
 
 document.getElementById('demo1').innerHTML =
     " The firstt paragraph is (index 0) inside : " + y[0].innerHTML;
+
+
+
+
+// Finding HTML Elements by Class Name
+
+const a = document.getElementsByClassName("intro");
+
+document.getElementById("demo2").innerHTML =
+    `the firstt paragraph is (index 0) with class : ${a[0].innerHTML}`;
