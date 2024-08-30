c1f3ffab72f8b41a42bf05986688e806a8cac83d
w3schools-test <raeebikash@gmail.com>
2024-08-30 17:07:59 +0545

    Update domForms

--- JS HTML DOM/domForms.js
+++ JS HTML DOM/domForms.js
@@ -5,3 +5,19 @@ function validateForm() {
         return false;
     }
 }
+
+// validate Numbers
+
+function myfunction() {
+    // get the value of the input field with id = "numb"
+    let a = document.getElementById("numb").value;
+    // If x is not a number or less than one or greater than 10
+    let text;
+    if (isNaN(a) || a < 1 || a > 10) {
+        text = "input not valid";
+    }
+    else {
+        text = "input is valid";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
