fa4fd76db3801cdda441fbf846bc2d6177700323
w3schools-test <raeebikash@gmail.com>
2024-08-30 14:37:29 +0545

    Update domElements

--- JS HTML DOM/domElements.js
+++ JS HTML DOM/domElements.js
@@ -7,3 +7,15 @@ const element = document.getElementById("intro");
 
 document.getElementById("demo").innerHTML = 
     `The text from the intro paragraph is  ${element.innerHTML}`;
+
+
+
+
+
+// Finding HTML Elements by Tag Name
+    
+const x = document.getElementById("main");
+const y = x.getElementsByTagName("p");
+
+document.getElementById('demo1').innerHTML =
+    " The firstt paragraph is (index 0) inside : " + y[0].innerHTML;
