7bc062fcc173ff3b5c12f301b1561aa7c411aa19
w3schools-test <raeebikash@gmail.com>
2024-08-30 16:00:30 +0545

    Update domHTML

--- JS HTML DOM/domHtml.js
+++ JS HTML DOM/domHtml.js
@@ -6,3 +6,8 @@ document.getElementById('p1').innerHTML = "New text";
 // Chaning the value of an Attribute
 
 document.getElementById("intro").style.color = "blue";
+
+
+// Dynamic HTML Content
+
+document.getElementById("demo").innerHTML = "Date: " + new Date();
