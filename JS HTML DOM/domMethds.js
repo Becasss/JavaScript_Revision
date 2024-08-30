db8bbb93bd205266a444bd471e8d3451e3082756
w3schools-test <raeebikash@gmail.com>
2024-08-30 13:36:40 +0545

    Update domMethods.js

--- JS HTML DOM/domMethds.js
+++ JS HTML DOM/domMethds.js
@@ -7,3 +7,19 @@ document.getElementById("demo").innerHTML = "Hello, world!";
 
 // innerHTML property can be used to get or change any HTML element, including <html> and <body>.
 
+
+
+// The HTML DOM Document Object
+
+
+// Finding HTML Elements
+/*
+document.getElementById(id)     - Find an element by element id
+document.getElementsByTagName(name)   - Find elements by tag name
+document.getElementsByClassName(name) - Find elements by class name
+*/
+
+
+// Changing HTML Elements
+/*
+element.innerHTML = new html content        -  Change the inner HTML of an element */
