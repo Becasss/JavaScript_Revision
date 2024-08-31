d209df982155629a075db1fa58c7458f921f3b94
w3schools-test <raeebikash@gmail.com>
2024-08-31 23:22:32 +0545

    Update domEvent.js

--- JS HTML DOM/domEvent.js
+++ JS HTML DOM/domEvent.js
@@ -1,3 +1,12 @@
 function changeText(id) {
     id.innerHTML = "Oops!";
 }
+
+
+
+
+// HTML Event Attributes
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = "Date()";
+}
