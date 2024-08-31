f48860210f45633f2f7c72f2cb43832bdcf71384
w3schools-test <raeebikash@gmail.com>
2024-08-31 23:48:17 +0545

    Update domEvent.js

--- JS HTML DOM/domEvent.js
+++ JS HTML DOM/domEvent.js
@@ -18,3 +18,16 @@ document.getElementById("myBtn").onclick = displayDate;
 function displayDate(){
 document.getElementById("demo1").innerHTML = Date();
 }
+
+
+// The onload Attribute
+
+function checkCookies() {
+    let text = "";
+    if (navigator.cookieEnabled == true) {
+        text = "Cookies are enabled.";
+    } else {
+        text = "Cookies are disabled.";
+    } document.getElementById("demo2").innerHTML
+        = text;
+}
