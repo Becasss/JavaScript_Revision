8e34cecef59779dc0464ca700caf65115daac8b2
w3schools-test <raeebikash@gmail.com>
2024-09-01 11:18:30 +0545

    Update domeventLIstener.js

---
+++ JS HTML DOM/domEventListener.js
@@ -0,0 +1,7 @@
+// The addEventListener() Method
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
