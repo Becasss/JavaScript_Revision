a58f9bc49b9dddb3b03ea1811af4e5bca22e6eae
w3schools-test <raeebikash@gmail.com>
2024-09-01 13:26:54 +0545

    Update domNavigation

--- JS HTML DOM/domNavigation.js
+++ JS HTML DOM/domNavigation.js
@@ -2,3 +2,16 @@ document.getElementById("id02").innerHTML = document.getElementById("id01").inne
 
 
 document.getElementById("id04").innerHTML = document.getElementById("id03").firstChild.nodeValue;
+
+
+document.getElementById('id06').innerHTML = document.getElementById("id05").childNodes[0].nodeValue;
+
+
+
+// DOM Root Nodes
+
+document.getElementById("id07").innerHTML = document.body.innerHTML;
+
+
+// Node Name Property
+document.getElementById("id08").innerHTML = document.getElementById("id09").nodeName;
