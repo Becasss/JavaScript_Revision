841487e5b403fa8fe379e773eb8f41145e3e4361
w3schools-test <raeebikash@gmail.com>
2024-09-01 14:14:36 +0545

    Update domNodes.js

--- JS HTML DOM/domNodes.js
+++ JS HTML DOM/domNodes.js
@@ -6,3 +6,15 @@ para.appendChild(node);
 
 const element = document.getElementsById("div1");
 element.appendChild(para);
+
+
+
+// Creating new HTML Elements - insertBefore()
+
+const newPara = document.createElement("p");
+const nodee = document.createTextNode("This is new.");
+newPara.appendChild(nodee);
+
+const elementt = document.getElementById("div2");
+const child = document.getElementById("p3");
+elementt.insertBefore(newPara, child);
