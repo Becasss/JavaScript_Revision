085f6876372547a6afedcc6bf5922270c600d200
w3schools-test <raeebikash@gmail.com>
2024-09-01 13:16:24 +0545

    Update domNavigation.js

--- JS HTML DOM/domNavigation.js
+++ JS HTML DOM/domNavigation.js
@@ -1,1 +1,4 @@
 document.getElementById("id02").innerHTML = document.getElementById("id01").innerHTML;
+
+
+document.getElementById("id04").innerHTML = document.getElementById("id03").firstChild.nodeValue;
