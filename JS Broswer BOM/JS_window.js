2edc6c1e9d341e98d8330cf021da7410ac8d9172
w3schools-test <raeebikash@gmail.com>
2024-09-02 12:06:57 +0545

    Update JS_window.js

---
+++ JS Broswer BOM/JS_window.js
@@ -0,0 +1,5 @@
+// Window size
+
+document.getElementById("demo").innerHTML = 
+    'Browser inner window width:  ' + window.innerWidth + 'px<br>' + 
+"Browser inner window height: " + window.innerHeight + 'px<br>;
