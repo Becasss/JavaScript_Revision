f70e58d70f344bd10190e8f08f6b7b2221e7c397
w3schools-test <raeebikash@gmail.com>
2024-09-03 20:02:10 +0545

    Updating JSONParse.js

--- JS JSON/jsonParse.js
+++ JS JSON/jsonParse.js
@@ -12,3 +12,14 @@ const text = '["Ford", "BMW", "Audi", "Fiat"]';
 const myArr = JSON.parse(text);
 
 console.log(myArr);
+
+
+
+// Exceptions : Converting a string into a date:
+
+const date = '{"name":"John","birth":"1986-12-14", "age":20, "city":"New York"}';
+
+const bdate = JSON.parse(date);
+
+console.log(bdate); // Throws error: Invalid Date
+console.log(bdate.name);
