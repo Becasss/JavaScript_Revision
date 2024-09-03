6cb13172c3565976311e4db75dc02f57fc122ae4
w3schools-test <raeebikash@gmail.com>
2024-09-03 20:44:11 +0545

    Update jsonstringify.js

--- JS JSON/jsonStringify.js
+++ JS JSON/jsonStringify.js
@@ -5,3 +5,28 @@ const obj = { name: "John", age: 30, city: "New York" };
 const myJSON = JSON.stringify(obj);
 
 console.log(myJSON);
+
+
+// Stringify a Javascript Array
+
+const arr = ["John", "John", "John", "John", "John"];
+
+const mySON = JSON.stringify(arr);
+
+console.log(mySON);
+
+
+
+// Storing Data
+
+const myObj = { name: "John", age: 30, city: "New York" };
+const myjo = JSON.stringify(myObj);
+
+localStorage.setItem("testJSON", myjo);
+
+
+// Retrieving Data:
+let text = localStorage.getItem("testJSON");
+let z = JSON.parse(text);
+
+console.log(z.name);
