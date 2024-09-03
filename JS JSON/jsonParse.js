a24d9d9ac4d69ec126f8050256900ad8cb8aae3f
w3schools-test <raeebikash@gmail.com>
2024-09-03 19:58:18 +0545

    update JsonParse.js

--- JS JSON/jsonParse.js
+++ JS JSON/jsonParse.js
@@ -3,3 +3,12 @@
 const obj = JSON.parse('{"name":"John","age":20, "city":"New York"}');
 console.log(obj);
 
+
+
+// Array as JSON
+
+const text = '["Ford", "BMW", "Audi", "Fiat"]';
+
+const myArr = JSON.parse(text);
+
+console.log(myArr);
