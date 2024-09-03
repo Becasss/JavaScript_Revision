f29b2c730d2b512481b61e90ef19046b9b839f77
w3schools-test <raeebikash@gmail.com>
2024-09-03 21:00:52 +0545

    Update jsonObjects.js

---
+++ JS JSON/jsonObjects.js
@@ -0,0 +1,13 @@
+// Javascript Objects:
+
+const myJSON = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "car": null
+
+};
+
+const myObj = JSON.parse(myJSON);
+
+console.log(myObj.name);
