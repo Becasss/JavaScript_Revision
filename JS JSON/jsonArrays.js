e1b4cbaee8cc44df0c15710617e94c307a24d6cc
w3schools-test <raeebikash@gmail.com>
2024-09-03 23:41:46 +0545

    update

--- JS JSON/jsonArrays.js
+++ JS JSON/jsonArrays.js
@@ -5,3 +5,13 @@ const myJSON = '["FOrd", "BMW", "Fiat"]';
 const myArray = JSON.parse(myJSON);
 
 console.log(myArray);
+
+
+// Arrays in Objects
+
+const obj = '{"name":"Bikash", "age":30,"cars": ["Ford", "BMW", "Fiat"]}';
+
+const myarr = JSON.parse(obj);
+
+console.log(myarr.cars);
+console.log(myarr.cars[0]);
--- JS JSON/jsonObjects.js
+++ JS JSON/jsonObjects.js
@@ -9,3 +9,18 @@ console.log(myObj.name);
 // Accessing Object Values:
 
 console.log(myObj.city);
+
+
+// Looping through array
+
+const a = '{"name":"John", "age":30, "car":["Ford","BMW","Fiat"]}';
+const b = JSON.parse(a);
+
+let text = '';
+for (let i in b.car) {
+    text += b.car[i] + ',';
+
+}
+
+
+console.log(text);
